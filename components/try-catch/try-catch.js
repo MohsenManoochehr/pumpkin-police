a48ee101840fb3470f413bcb34bc52281f2f57e6
mohsen <mohsenmanoochehr@gmail.com>
2025-08-12 11:41:59 +0330

    TryCatch updated

--- components/try-catch/try-catch.js
+++ components/try-catch/try-catch.js
@@ -1,13 +1,33 @@
+// utils/TryCatch.js (CommonJS)
 const { Catch } = require("../catch/Catch");
 
-function TryCatch(code) {
+function toErrorProps(err, ctx) {
+  return {
+    name: err?.name,
+    message: err?.message || String(err),
+    stack: err?.stack,
+    ...ctx, // optional metadata you pass (route, userId, etc.)
+  };
+}
+
+/**
+ * TryCatch(codeOrPromise, ctx?, opts?)
+ * - codeOrPromise: () => any | Promise<any> | Promise<any>
+ * - ctx: optional extra fields to include in errorProps
+ * - opts: { rethrow?: boolean } default false
+ */
+async function TryCatch(codeOrPromise, ctx = {}, opts = {}) {
   try {
-    code();
-  } catch (error) {
-    Catch(error);
+    const p =
+      typeof codeOrPromise === "function"
+        ? Promise.resolve().then(codeOrPromise)
+        : Promise.resolve(codeOrPromise);
+    return await p;
+  } catch (err) {
+    await Catch(toErrorProps(err, ctx));
+    if (opts.rethrow) throw err; // optionally bubble up
+    return undefined;
   }
 }
 
-module.exports = {
-  TryCatch,
-};
+module.exports = { TryCatch };
