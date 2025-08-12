e8c4cf23e9185ddc71886084b37f3ff856001e30
mohsen <mohsenmanoochehr@gmail.com>
2025-08-12 11:46:54 +0330

    loadConfig updated

--- components/config/configLoader.js
+++ components/config/configLoader.js
@@ -1,14 +1,41 @@
+// config/configLoader.js (CommonJS)
 const { cosmiconfig } = require("cosmiconfig");
+const path = require("path");
+
+function getSearchFrom() {
+  // Prefer an explicit override first
+  if (process.env.POLICEOFFICE_CONFIG_FROM)
+    return process.env.POLICEOFFICE_CONFIG_FROM;
+
+  // Electron? Use userData for installed apps; cwd for dev
+  try {
+    const { app } = require("electron");
+    if (app) {
+      // If this throws because app not initialized, we'll fall back
+      return app.isPackaged ? app.getPath("userData") : process.cwd();
+    }
+  } catch {
+    /* not running in Electron main */
+  }
+
+  return process.cwd();
+}
 
 async function loadConfig() {
   const explorer = cosmiconfig("policeoffice", {
     searchPlaces: ["police-office.json"],
   });
 
-  const result = await explorer.search();
-  return result?.config ?? {};
+  // If the user wants to point to an exact file, allow it:
+  if (process.env.POLICEOFFICE_CONFIG_FILE) {
+    const res = await explorer.load(
+      path.resolve(process.env.POLICEOFFICE_CONFIG_FILE)
+    );
+    return res?.config ?? {};
+  }
+
+  const res = await explorer.search(getSearchFrom());
+  return res?.config ?? {};
 }
 
-module.exports = {
-  loadConfig,
-};
+module.exports = { loadConfig };
